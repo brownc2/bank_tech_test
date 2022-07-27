b216bef26793277a663497ff0b5fcb2601b81bb7
Christopher <christopher2brown@hotmail.co.uk>
2022-07-27 11:27:55 +0100

    completed bank tech test

--- bank_balance.js
+++ bank_balance.js
@@ -1,49 +1,51 @@
-class Bank_balance {
-  
+class BankBalance {
   constructor() {
-    this.debit = 0
-    this.credit = 0
-    this.account = 0
-    this.statement = []
+    this.debit = 0;
+    this.credit = 0;
+    this.account = 0;
+    this.statement = [];
+    this.date = new Date().toLocaleDateString();
   }
-  
+  // This function allows the user to add funds to the account.
   deposit(user_input) {
     const date = new Date().toLocaleDateString();
     this.credit = user_input;
-    this.account += this.credit
-    this.statement.unshift({
-      date: date,
-      credit: user_input,
-      debit: null,
-      balance: this.account,
-    });
-
-  }  
-   
+    this.account += this.credit;
+    this.statement.unshift(
+      `\n${date} || ${this.credit}.00 ||  || ${this.account}.00`
+    );
+  }
+  // This function allows the user to withdraw funds from the account.
   withdraw(user_input) {
     const date = new Date().toLocaleDateString();
-
     if (this.account <= 0) {
-      throw new Error("Sorry you have insufficient funds")
-    }
-    else {
-    this.debit = user_input
-    this.account -= this.debit
-    this.statement.unshift({
-      date: date,
-      credit: null,
-      debit: this.debit,
-      balance: this.account,
-    });
-    console.log(this.statement)
+      throw new Error("Sorry you have insufficient funds");
+    } else {
+      this.debit = user_input;
+      this.account -= this.debit;
+      this.statement.unshift(
+        `\n${date} || || ${this.debit}.00 || ${this.account}.00`
+      );
     }
   }
-  
+  // This function allows us to view the current balance of the account.
   account_total() {
-    return this.account
-  }  
+    return this.account;
+  }
+  // This function allows us to view the statement of the account.
+  user_statement() {
+    let header = `date || credit || debit || balance`;
+
+    this.statement = this.statement.toString();
+    this.statement = this.statement.split(", ");
+
+    for (let index = 0; index < this.statement.length; ++index) {
+      const element = this.statement[index];
+      console.log(header);
+      console.log(element);
+      return (header += element);
+    }
+  }
 }
 
-module.exports = Bank_balance;
-
-
+module.exports = BankBalance;
--- bank_balance.test.js
+++ bank_balance.test.js
@@ -1,71 +1,60 @@
-const Bank_balance = require('./bank_balance')
+const BankBalance = require("./bank_balance");
 
-describe('bank_balance', () => {
-  it('Will add 10 to the bank balance', () => {
-   const bank = new Bank_balance();        
-   bank.deposit(10);
-   expect(bank.account_total()).toBe(10)
-  
+describe("bank_balance", () => {
+  it("Will add 10 to the bank balance", () => {
+    const bank = new BankBalance();
+    bank.deposit(10);
+    expect(bank.account_total()).toBe(10);
   });
 
-  it('Will add two lots of 10 to the bank balance', () => {
-    const bank = new Bank_balance();        
+  it("Will add two lots of 10 to the bank balance", () => {
+    const bank = new BankBalance();
     bank.deposit(10);
     bank.deposit(10);
-    expect(bank.account_total()).toBe(20)
+    expect(bank.account_total()).toBe(20);
   });
 
-    it('Will add two lots of 10 to the bank balance and remove 10', () => {
-      const bank = new Bank_balance();        
-      bank.deposit(10);
-      bank.deposit(10);
-      bank.withdraw(10)
-      expect(bank.account_total()).toBe(10)
-     
- 
-   });
+  it("Will add two lots of 10 to the bank balance and remove 10", () => {
+    const bank = new BankBalance();
+    bank.deposit(10);
+    bank.deposit(10);
+    bank.withdraw(10);
+    expect(bank.account_total()).toBe(10);
+  });
 
-   it('Will add 100 to the bank balance and remove 50', () => {
-    const bank = new Bank_balance();        
+  it("Will add 100 to the bank balance and remove 50", () => {
+    const bank = new BankBalance();
     bank.deposit(10);
     bank.deposit(10);
     bank.deposit(10);
     bank.deposit(10);
     bank.deposit(60);
-    bank.withdraw(50)
-    expect(bank.account_total()).toBe(50)
- });
+    bank.withdraw(50);
+    expect(bank.account_total()).toBe(50);
+  });
 
- it('if the account is less than 0, it will throw a error', () => {
-  const bank = new Bank_balance();        
-  expect(() => bank.withdraw()).toThrow("Sorry you have insufficient funds")
-  expect(bank.account_total()).toBe(0)
- }); 
+  it("if the account is less than 0, it will throw a error", () => {
+    const bank = new BankBalance();
+    expect(() => bank.withdraw(50)).toThrow(
+      "Sorry you have insufficient funds"
+    );
+    expect(bank.account_total()).toBe(0);
+  });
 
- it('if the account has 200 in the account and you try to withdraw 50, no error will be shown', () => {
-  const bank = new Bank_balance();        
-  bank.deposit(200)
-  bank.withdraw(50);
-  expect(bank.account_total()).toBe(150)
- }); 
- it("shows a withdrawal, balance and date on statement", () => {
-  const bank = new Bank_balance(); 
-  const date = new Date().toLocaleDateString();
+  it("if the account has 200 in the account and you try to withdraw 50, no error will be shown", () => {
+    const bank = new BankBalance();
+    bank.deposit(200);
+    bank.withdraw(50);
+    expect(bank.account_total()).toBe(150);
+  });
 
-  bank.deposit(10);
-  bank.withdraw(10);
-
-  expect(bank.statement).toEqual([
-    { date: date,
-      credit: null,
-      debit: 10,
-      balance: 0,
-    },
-    { date: date,
-      credit: 10,
-      debit: null,
-      balance: 10,
-    },
-  ])
-})
+  it("will return a bank statement", () => {
+    const mockdate = new Date().toLocaleDateString();
+    const bank = new BankBalance();
+    bank.deposit(10);
+    bank.withdraw(10);
+    expect(bank.user_statement()).toEqual(
+      `date || credit || debit || balance\n${mockdate} || || 10.00 || 0.00,\n${mockdate} || 10.00 ||  || 10.00`
+    );
+  });
 });
